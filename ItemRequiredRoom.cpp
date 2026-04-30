2b4cc64e7b59f18363c5c16558a9f460d6c0efd6
YangJiseop <vastu54321@gmail.com>
2026-04-30 18:17:54 +0900

    Refactoring : ItemRequiredRoom클래스에서 기존에 vector<unique_ptr<Item>>& inventory를 순회하면서 동일한 이름을 찾던걸 find_if와 람다식을 사용하는 코드로 리팩토링함

--- ItemRequiredRoom.cpp
+++ ItemRequiredRoom.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include "ItemRequiredRoom.h"
+
 
 ItemRequiredRoom::ItemRequiredRoom(const string& name, const string& keyItemName)
     : Room(name), keyItemName(keyItemName){}
@@ -7,12 +9,10 @@ ItemRequiredRoom::~ItemRequiredRoom(){}
 
 bool ItemRequiredRoom::HasRequiredItem(const vector<unique_ptr<Item>>& inventory) const
 {
-    for (const auto& item : inventory)
+    auto it = find_if(inventory.begin(), inventory.end(), [this](const auto& item)
     {
-        if (item->GetItemName() == keyItemName)
-        {
-            return true;
-        }
-    }
-    return false;
+        return item->GetItemName() == keyItemName;
+    });
+    // end()가 아니라면 keyItemName과 일치하는 아이템을 찾았다는 뜻이므로 true, 못찾아서 end()까지 가면 false
+    return it != inventory.end();
 }
